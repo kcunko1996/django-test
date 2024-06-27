7de5bf37806b053907d08405290f033a07bf75d4
kcunko1996 <karlocunko1996@gmail.com>
2024-06-27 19:37:14 +0200

    feat: pipeline

--- backend/crud/deployment.py
+++ backend/crud/deployment.py
@@ -27,25 +27,25 @@ CORS_ALLOWED_ORIGINS = [
 
 STORAGES = {
     "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage"
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage"
-    }
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
 }
+
 
 CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 CONNECTION_STR = {pair.split('=')[0]: pair.split('=')[1]
                   for pair in CONNECTION.split(' ')}
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgrresql',
-        'NAME': CONNECTION_STR['dbname'],
-        'HOST': CONNECTION_STR['host'],
-        'USER': CONNECTION_STR['user'],
-        'PASSWORD': CONNECTION_STR['password'],
-
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": CONNECTION_STR['dbname'],
+        "HOST": CONNECTION_STR['host'],
+        "USER": CONNECTION_STR['user'],
+        "PASSWORD": CONNECTION_STR['password'],
     }
 }
 
