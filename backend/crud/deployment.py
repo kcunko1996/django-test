dc4389dc2d88acc8001eec29ac6bc602a1396d87
kcunko1996 <karlocunko1996@gmail.com>
2024-06-27 23:07:28 +0200

    feat: pipeline

--- backend/crud/deployment.py
+++ backend/crud/deployment.py
@@ -2,6 +2,7 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
+
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
