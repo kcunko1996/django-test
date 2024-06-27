634b64b83b2c1c7590ac9fab49beb13599f4b79a
kcunko1996 <karlocunko1996@gmail.com>
2024-06-27 19:14:12 +0200

    feat: pipeline

--- backend/crud/settings.py
+++ backend/crud/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -62,7 +63,7 @@ TEMPLATES = [
             ],
         },
     },
-] 
+]
 
 WSGI_APPLICATION = 'crud.wsgi.application'
 
@@ -118,3 +119,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Set STATIC_ROOT to an absolute path (example: 'path/to/your/staticfiles')
