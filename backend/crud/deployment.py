c6543190d9a99a1e9814b23ee654b4b30465db98
kcunko1996 <karlocunko1996@gmail.com>
2024-06-27 20:19:58 +0200

    feat: typo fix

--- backend/crud/deployment.py
+++ backend/crud/deployment.py
@@ -2,7 +2,6 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
